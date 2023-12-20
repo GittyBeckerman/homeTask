b5665c12dc740865b694b406c96c59b019a896d8
User <User@DESKTOP-SOMQ3KC>
2023-12-20 20:35:28 +0200

    Add project files.

---
+++ homeTask/GenerateCsvFile.cs
@@ -0,0 +1,72 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+     public partial class Program
+    {
+        public static string GenerateCsvFileFunc()
+        {
+            List<Person> people = GenerateRandomPeople(1000);
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(desktopPath, "people_dataset.csv");
+
+            var writer = new StreamWriter(filePath);
+            var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            csv.WriteRecords(people);
+            writer.Close();
+
+            Console.WriteLine($"generate CSV file succesfully, saved to: {filePath}");
+            return filePath;
+        }
+
+        static List<Person> GenerateRandomPeople(int count)
+        {
+            List<Person> people = new List<Person>();
+            Random random = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                string firstName = GetRandomFirstName();
+                string lastName = GetRandomLastName();
+                int age = random.Next(18, 71); // Random age between 18 and 70
+                double weight = random.NextDouble() * (300 - 100) + 100; // Random weight between 100 and 300 lbs
+                string gender = random.Next(2) == 0 ? "Male" : "Female";
+
+                people.Add(new Person
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age,
+                    Weight = Math.Round(weight, 2),
+                    Gender = gender
+                });
+            }
+
+            return people;
+        }
+
+        static string GetRandomFirstName()
+        {
+            string[] firstNames = { "Gitty", "Tamar", "Lea", "Efrat", "Noa" };
+            Random random = new Random();
+            return firstNames[random.Next(firstNames.Length)];
+        }
+
+        static string GetRandomLastName()
+        {
+            string[] lastNames = { "Doe", "Smith", "Johnson", "Williams", "Jones" };
+            Random random = new Random();
+            return lastNames[random.Next(lastNames.Length)];
+
+        }
+
+
+    }
+
---
+++ homeTask/Program.cs
@@ -0,0 +1,102 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public partial class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("hello! to enter csvFile Path enter 1,/n to generate automaticly csv file enter 2 /n , CancellationToken exit enter 0");
+                string info = Console.ReadLine();
+                if (info == "1")
+                {
+                    Console.WriteLine("please enter valid path of your csv file");
+                    string path = Console.ReadLine();
+                    List<Person> people = ReadCsvFile(path);
+                    CalculateAndPrintResults(people);
+
+                }
+                if (info == "2")
+                {
+                    string path = GenerateCsvFileFunc();
+                    List<Person> people = ReadCsvFile(path);
+                    CalculateAndPrintResults(people);
+
+                }
+                if (info == "0")
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("wrong output, please try again");
+                }
+
+                // Read CSV file and load data into a list of People
+                //rum the function that calculate the averages
+            }
+        }
+        catch(Exception e) {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+
+    public static  List<Person> ReadCsvFile(string filePath)
+    {
+        List<Person> people = new List<Person>();
+
+        var reader = new StreamReader(filePath);
+            reader.ReadLine(); // Skip header
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                people.Add(new Person
+                {
+                    FirstName = values[0],
+                    LastName = values[1],
+                    Age = int.Parse(values[2]),
+                    Weight = double.Parse(values[3]),
+                    Gender = values[4]
+                });
+            }
+        
+
+        return people;
+    }
+
+    public  static void CalculateAndPrintResults(List<Person> people)
+    {
+        // Calculate average age of all people
+        double averageAge = people.Average(p => p.Age);
+        Console.WriteLine($"Average Age of All People: {averageAge:F2}");
+
+        // Count people weighing between 120lbs and 140lbs
+        int countWeightBetween120And140 = people.Count(p => p.Weight >= 120 && p.Weight <= 140);
+        Console.WriteLine($"Total People Weighing between 120lbs and 140lbs: {countWeightBetween120And140}");
+
+        // Calculate average age of people weighing between 120lbs and 140lbs
+        double averageAgeWeightBetween120And140 = people
+            .Where(p => p.Weight >= 120 && p.Weight <= 140)
+            .Average(p => p.Age);
+        Console.WriteLine($"Average Age of People Weighing between 120lbs and 140lbs: {averageAgeWeightBetween120And140:F2}");
+    }
+}
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public double Weight { get; set; }
+    public string Gender { get; set; }
+}
